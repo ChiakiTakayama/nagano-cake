ca545c1aeb7d63dfc5c663ba70152ada5a1c2ec5
ChiakiTakayama <106967678+ChiakiTakayama@users.noreply.github.com>
2022-09-19 12:14:51 +0000

    Order_detailモデル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_09_19_081723) do
+ActiveRecord::Schema.define(version: 2022_09_19_120739) do
 
   create_table "addresses", force: :cascade do |t|
     t.integer "customer_id"
@@ -60,6 +60,16 @@ ActiveRecord::Schema.define(version: 2022_09_19_081723) do
     t.text "introduction"
     t.integer "price"
     t.boolean "is_active", default: true, null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "order_details", force: :cascade do |t|
+    t.integer "order_id"
+    t.integer "item_id"
+    t.integer "amount"
+    t.integer "price"
+    t.integer "making_status", default: 0
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
