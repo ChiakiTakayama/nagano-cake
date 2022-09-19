60a93f007be7fb21ff15eebd4fe65339b2b0aa58
ChiakiTakayama <106967678+ChiakiTakayama@users.noreply.github.com>
2022-09-19 08:52:53 +0000

    Orderモデルのマイグレーションファイルの編集中

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   devise_for :customers
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get 'top' => 'homes#top'
+ 
+ root to:'homes#top'
   
   get 'items' => 'public/items#index'
   
---
+++ db/migrate/20220919081723_create_orders.rb
@@ -0,0 +1,15 @@
+class CreateOrders < ActiveRecord::Migration[6.1]
+  def change
+    create_table :orders do |t|
+      t.integer :customer_id
+      t.string :name
+      t.string :postal_code
+      t.string :address
+      t.integer :postage
+      t.integer :price
+      t.integer :payment_method
+
+      t.timestamps
+    end
+  end
+end
