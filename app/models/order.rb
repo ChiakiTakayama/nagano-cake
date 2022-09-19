ef6e0dc43864e16c86964de4e8de1fa91491b594
ChiakiTakayama <106967678+ChiakiTakayama@users.noreply.github.com>
2022-09-19 12:43:10 +0000

    orderモデルにenum記述

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,6 @@
 class Order < ApplicationRecord
+  enum :payment_method, [ クレジットカード, 銀行振込 ]
+  # enum :payment_method, [ 0:クレジットカード, 1:銀行振込 ]の可能性あり
+
+  enum :status, [ 入金待ち, 入金確認, 製作中, 発送準備中, 発送済み]
 end
